09277ce326b291233ad0b7fa02bdebb4cf2179fe
amrhossamdev <amrhossam12345@gmail.com>
2018-09-08 04:14:44 +0200

    Ui design

---
+++ app/src/main/java/com/amrhossam/firebasenewsapp/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package com.amrhossam.firebasenewsapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amrhossam.firebasenewsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUpUser();
+        loginUser();
+    }
+
+    private void signUpUser(){
+        // your code
+    }
+
+    private void loginUser(){
+
+        // your code
+    }
+}
